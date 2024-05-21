04253af795f93fe14e188356bf14d71f41cbee9f
Karatsu-life <169158813+Karatsu-life@users.noreply.github.com>
2024-05-21 15:29:29 +0200

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -21,7 +21,7 @@
 
 
   
-  // image partie blog ( ouverture popup)
+  // image partie blog  img 1 ( ouverture popup)
   $(document).ready(function() {
 
     function showImage(fullPath , fullPath2) {
@@ -30,45 +30,47 @@
             var fullImagePath =
                 fullPath;
         }
-        if (fullPath2 == "image2") {
-            var fullImagePath =
-                fullPath2;
-        }
-        if (fullPath == "image3") {
-            var fullImagePath =
-                fullPath;
-        }
-        if (fullPath == "image4") {
-            var fullImagePath =
-                fullPath;
-        }
-        if (fullPath == "image5") {
-            var fullImagePath =
-                fullPath;
-        }
-
-
         $('.image').attr({
             'src': fullImagePath
         });
-
         //if close button is clicked
         $('.window .close').click(function(e) {
             //Cancel the link behavior
             e.preventDefault();
-
             $('#mask').removeClass('showing');
         });
-
     };
-
     $(".showImage").on("click", function() {
         showImage($(this).text());
         $('#mask').addClass('showing');
-
     });
-  
+  });
 
-});
+
+ // image partie blog  img 2 ( ouverture popup)
+  $(document).ready(function() {
+
+    function showImage2(fullPath) {
+        var id = '#dialog';
+        if (fullPath == "image") {
+            var fullImagePath =
+                fullPath;
+        }
+        $('.image').attr({
+            'src': fullImagePath
+        });
+        //if close button is clicked
+        $('.window .close').click(function(e) {
+            //Cancel the link behavior
+            e.preventDefault();
+            $('#mask').removeClass('showing');
+        });
+    };
+    $(".showImage").on("click", function() {
+        showImage($(this).text());
+        $('#mask').addClass('showing');
+    });
+  });
+  
   
 })(jQuery);
