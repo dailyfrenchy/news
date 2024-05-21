102718b73fb3d713262d12298aadda3c2563a5d8
Karatsu-life <169158813+Karatsu-life@users.noreply.github.com>
2024-05-21 16:30:37 +0200

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -24,7 +24,7 @@
   // image partie blog  img 1 ( ouverture popup)
   $(document).ready(function() {
 
-    function showImage(fullPath , fullPath2) {
+    function showImage(fullPath) {
         var id = '#dialog';
         if (fullPath == "image") {
             var fullImagePath =
